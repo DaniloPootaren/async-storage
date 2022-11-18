7ecd92ca82206bb33ed99a92781648ebbb38c630
Jean Danilo Maxwell Patrick Pootaren <62770019+Daniloxax@users.noreply.github.com>
2022-11-18 16:59:13 +0400

    Added design for login page

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {
+  Center,
+  Box,
+  Heading,
+  VStack,
+  FormControl,
+  Button,
+  Input,
+} from 'native-base';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,7 +21,41 @@ const styles = StyleSheet.create({
 export const LoginScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Login Screen</Text>
+      <Center w="100%">
+        <Box safeArea p="2" py="8" w="90%" maxW="290">
+          <Heading
+            size="lg"
+            fontWeight="600"
+            color="coolGray.800"
+            _dark={{
+              color: 'warmGray.50',
+            }}>
+            Welcome
+          </Heading>
+          <Heading
+            mt="1"
+            _dark={{
+              color: 'warmGray.200',
+            }}
+            color="coolGray.600"
+            fontWeight="medium"
+            size="xs">
+            Sign in to continue!
+          </Heading>
+
+          <VStack space={3} mt="5">
+            <FormControl>
+              <FormControl.Label>Email Address</FormControl.Label>
+              <Input />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Password</FormControl.Label>
+              <Input type="password" />
+            </FormControl>
+            <Button mt="2">Login</Button>
+          </VStack>
+        </Box>
+      </Center>
     </View>
   );
 };
